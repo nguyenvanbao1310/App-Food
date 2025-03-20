7393cc432b7a1b702c439af3eeb74e13a4a2ac2d
nguyenvanbao <nguyenhoanggiaphong12@gmail.com>
2025-03-20 13:22:34 +0700

    repository

---
+++ API/API_AppFood/src/main/java/com/example/api_appfood/ApiAppFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_appfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAppFoodApplication.class, args);
+    }
+
+}
---
+++ API/API_AppFood/src/test/java/com/example/api_appfood/ApiAppFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_appfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAppFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
